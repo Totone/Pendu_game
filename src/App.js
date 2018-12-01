6d43a20e5c74de8028a281722299afd9369eb968
Antho <antho.oclock@gmail.com>
2018-12-01 17:43:51 +0100

    finitions

--- src/App.js
+++ src/App.js
@@ -1,48 +1,57 @@
 import React, { Component } from 'react';
-import Popup from 'reactjs-popup';
 import logo from './logo.svg';
 import './App.css';
 
 import Screen from './Screen';
-import './Keyboard.css';
 import Key from './Key';
 import Letter from './Letter';
 
 
-const NB_ATTEMPTS = 10;
-const WORDS = [
-    'PARI', 
-    'JUVENILE', 
-    'TOXICO', 
-    'PHILANTROPIQUE', 
-    'BAZAR', 
-    'ZIDAAAANE',
-    'FOLKLORE', 
-    'PARASITE', 
-    'GENIE'
-]
-
 class App extends Component {   
     constructor(props) {
         super(props);
-        const newWord = WORDS[Math.floor(Math.random()*WORDS.length)];
+        const words = [
+            'PARI', 
+            'JUVENILE', 
+            'TOXICO', 
+            'PHILANTROPIQUE', 
+            'BAZAR', 
+            'ZIDAAAANE',
+            'FOLKLORE', 
+            'PARASITE', 
+            'GENIE',
+            'MALSAIN', 
+            'PURIFIER',
+            'CRISTALINE',
+            'SOURCE',
+            'TAMBOURINNER'
+        ]
+        const newWord = words[Math.floor(Math.random()*words.length)];
 
         this.state = {
+            nbTries: 0,
             nbErrors: 0,
             gameState: 'PLAY',
             triedLetters: [],
             goodLetters: Array.from(newWord),
+
             keysArray: ['ABCDEFGHIJKLM', 'NOPQRSTUVWXYZ'],
+            nbAttempts: 10,
+            words: words,
+
         }
     }
 
     /**
-     * Réinitialiser le state en cas de victoire ou de défaite
+     * Réinitialiser le state en cas de victoire, défaite ou pour recommencer une partie
+     * Fonction fléchée pour garantir le bon comportement (event)
      */
     initState = () => {
-        const newWord = WORDS[Math.floor(Math.random()*WORDS.length)];
+        const {words} = this.state;
+        const newWord = words[Math.floor(Math.random()*words.length)];
         const arrayWord = Array.from(newWord);
         this.setState({
+            nbTries: 0,
             nbErrors: 0,
             triedLetters: [],
             goodLetters: arrayWord,
@@ -53,23 +62,19 @@ class App extends Component {
 
     /**
      * Vérifier l'état du jeu
-     * Envoie une popup qui dit si t'as perdu ou gagné selon le cas
-     * 
-     * 3 cas
-     *      Le nombre d'erreurs est supérieur ou égal à 10
-     *          Faire une popup pour dire "perdu"
-     *      On vérifie pour chaque letter si sa valeur est dans triedLetters
-     *          Si c'est bon on fait une popup pour dire "gagné"
-     *      Sinon ya R on continue
+     * Bloquer l'avancement du jeu et envoyer une popup en cas de défaite ou de victoire
      */
     isFinished() {
-        const {nbErrors, triedLetters, goodLetters} = this.state;
+        const {nbErrors, nbTries, triedLetters, goodLetters} = this.state;
+
+        // vérification de la défaite
         if (nbErrors >= 10) {
             this.setState({gameState: 'LOSE'});
-            alert("T'as perdu t'es nul");
+            alert(`Vous avez perdu! Le mot à trouver était ${goodLetters.join('')}, vous aurez plus de chance à la prochaine partie!`);
         }
         
-        var valid = 0;
+        // incrémentation d'une variable à chaque bonne lettre dans triedLetters[] pour valider la victoire
+        let valid = 0;
         for (var x in goodLetters) {
             for(var y in triedLetters) {
                 if (goodLetters[x] === triedLetters[y]) {
@@ -77,27 +82,24 @@ class App extends Component {
                 }
             }
         }
-        console.log(`${valid} réussites sur ${goodLetters.length}`);
         if (valid === goodLetters.length) {
             this.setState({gameState: 'WIN'});
-            alert("T'as gagné t'es trop fort");
+            alert(`Vous avez réussi! Vous avez trouvé le mot ${goodLetters.join('')} en ${nbTries} tentatives. Félicitations!`);
         }
     }
     /**
      * Réaction au clic d'une key
-     * 
-     * Si la key est déjà cliquée y'a rien à faire 
-     * Sinon on ajoute la key dans triedLetters[]
-     * et on ajoute une erreur si la key n'est pas dans goodLetters[]
-     * 
-     * A faire:
-     * - état du jeu (gagné?perdu?) à la fin
-     *      appeler une nouvelle fonction et faire tous les bails de vérif
+     * Fonction fléchée pour garantir le bon comportement (event)
      */
     handleKeyClick = (letter, feedback) => {
-        const { gameState, goodLetters, triedLetters, nbErrors } = this.state;
+        const { gameState, goodLetters, triedLetters, nbErrors, nbTries } = this.state;
 
+        //produit un effet seulement si on joue et qu'on clique sur une key non cliquée
         if (gameState === 'PLAY' && feedback === 'unclicked') {
+            // incrémentation du compteur de tentatives
+            const newNbTries = nbTries+1;
+            this.setState({nbTries: newNbTries});
+
             // ajout de la lettre à triedLetters[]
             const newArray = triedLetters;
             newArray.push(letter);
@@ -111,69 +113,67 @@ class App extends Component {
             // ajout d'une erreur si nécessaire
             const newErrors = isGood ? nbErrors : nbErrors + 1;
 
-            // setState
+            // update
             this.setState({
                 triedLetters: newArray.sort(),
                 nbErrors: newErrors,
             });
 
-            console.log(triedLetters, this.state.nbErrors)
+            // vérification de l'état du jeu
             this.isFinished();
         }
     }
 
     /**
-     * Afficher les buttons correspondant aux lettres
-     * 
-     * 3 possibilités:
-     *  - si la valeur du button n'est pas dans triedLetters[] 
-     *      on return 'unclicked'
-     *  - sinon:
-     *      - si la valeur du button est dans goodLetters[]
-     *          on return 'success'
-     *      - sinon on return 'failure'
+     * Récupérer le feedback des lettres pour savoir comment les afficher
+     * @param letter: key ciblée
      */
     getFeedbackForKey(letter) {
         const {triedLetters, goodLetters} = this.state;
 
         for (var val1 in triedLetters) {
             if (letter === triedLetters[val1]) {
+                // Si la lettre est dans triedLetters[]
                 for (var val2 in goodLetters) {
                     if (letter === goodLetters[val2]) 
+                    // ET dans goodLetters[] -> success
                         return 'success';
                 }
+                // Si elle n'est que dans triedLetters[] -> failure
                 return 'failure';
             }
         }
+        // Si elle n'est dans aucun tableau -> unclicked
         return 'unclicked';
     }
 
     /**
-     * Donner le feedback de chaque lettre du mot à trouver
-     * 
-     * 2 possibilités
-     *      La lettre est dans triedLetters[]
-     *          return "known"
-     *      La lettre n'y est pas
-     *          return "unknown"
+     * Donner le feedback des lettres du mot à trouver pour savoir comment les afficher
+     * @param letter: lettre ciblée
      */
     getFeedbackForLetter(letter) {
         const {triedLetters} = this.state;
 
         for (var value in triedLetters) {
             if (letter === triedLetters[value]) {
+                // Si la lettre est dans triedLetters[] -> known
                 return "known";
             }
         }
+        // Si elle n'y est pas -> unknown
         return "unknown";
     }
 
     render() {
-        const { keysArray, nbErrors, goodLetters, gameState } = this.state;
+        const { keysArray, nbErrors, nbAttempts, goodLetters, gameState } = this.state;
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img 
+                        src={logo} 
+                        className="App-logo" 
+                        alt="logo" 
+                    />
                     <p>
                         Application créée avec <a
                           className="App-link"
@@ -191,18 +191,18 @@ class App extends Component {
                 <article id="wrapper">
                     <section>
                         <Screen
-                        nbAttempts={NB_ATTEMPTS} 
-                        nbErrors={nbErrors}
-                        gameState={gameState}
+                            nbAttempts={nbAttempts} 
+                            nbErrors={nbErrors}
+                            gameState={gameState}
                         />
 
                         <div className="expression">
                             {
                                 goodLetters.map((letter, index) => (
                                     <Letter
-                                    key={index}
-                                    letter={letter}
-                                    feedback={this.getFeedbackForLetter(letter)}
+                                        key={index}
+                                        letter={letter}
+                                        feedback={this.getFeedbackForLetter(letter)}
                                     />
                                 ))
                             }
@@ -214,10 +214,10 @@ class App extends Component {
                             { 
                                 Array.from(keysArray[0]).map((value) => (    
                                     <Key
-                                    key={value}
-                                    letter={value} 
-                                    feedback={this.getFeedbackForKey(value)}
-                                    clickEvent={this.handleKeyClick}
+                                        key={value}
+                                        letter={value} 
+                                        feedback={this.getFeedbackForKey(value)}
+                                        clickEvent={this.handleKeyClick}
                                     />
                                 ))
                             }
@@ -226,18 +226,18 @@ class App extends Component {
                             {
                                 Array.from(keysArray[1]).map((value) => (    
                                     <Key
-                                    key={value}
-                                    letter={value} 
-                                    feedback={this.getFeedbackForKey(value)}
-                                    clickEvent={this.handleKeyClick}
-                                />
+                                        key={value}
+                                        letter={value} 
+                                        feedback={this.getFeedbackForKey(value)}
+                                        clickEvent={this.handleKeyClick}
+                                    />
                                 ))
                             }
                         </div>
                         
                         <button 
-                        className={gameState === 'PLAY' ? 'restart' : 'restartFinished'}
-                        onClick={this.initState}
+                          className={gameState === 'PLAY' ? 'restart' : 'restartFinished'}
+                          onClick={this.initState}
                         >
                             Recommencer
                         </button>
